f74bde052452c2c392ce36ecc623566d28dab959
Carlos Febres <febres0492@gmail.com>
2024-03-27 22:26:51 -0400

    fixing theme toggler btn

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,19 +2,33 @@
 // getting the theme from local storage
 let theme = localStorage.getItem('blog-theme') || 'light';
 
+S('.theme-toggle')[0].textContent = 'Dark';
+
 // setting the theme
 if (theme && theme === 'dark') {
     document.body.classList.add('dark-theme');
+    S('.theme-toggle')[0].textContent = 'Light';
 }
 
+
 // toggling the theme
-document.querySelector('.theme-toggle').addEventListener('click', () => {
+S('.theme-toggle')[0].addEventListener('click', (ev) => {
     theme = localStorage.getItem('blog-theme');
     if (theme === 'light') {
         document.body.classList.add('dark-theme');
         localStorage.setItem('blog-theme', 'dark');
+        S('.theme-toggle')[0].textContent = 'Light';
     }else{
         document.body.classList.remove('dark-theme');
         localStorage.setItem('blog-theme', 'light');
+        S('.theme-toggle')[0].textContent = 'Dark';
     }
 });
+
+function S(selector) {
+    return document.querySelectorAll(selector);
+}
+function capFirst(str) {
+    if (!str) return str;
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
